362ab3f83fe73c247a5a63079430b4001ac9086d
TheWincher <louchez26@hotmail.fr>
2019-11-29 15:23:39 +0100

    Ajout du menu

---
+++ Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject mainMenu, playMenu;
+    public Button play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(Play);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Play()
+    {
+        playMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+}
